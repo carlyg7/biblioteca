ef4c34889318990cbe42f9d67662d5aaa9ecdad4
joarodmar2 <joaquinrodriguez1117@gmail.com>
2024-04-04 13:50:44 +0200

    prueba2

--- app/src/main/java/com/example/biblioteca_cm/MainCatalogo.java
+++ app/src/main/java/com/example/biblioteca_cm/MainCatalogo.java
@@ -12,7 +12,9 @@ public class MainCatalogo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.catalogo);
+        setContentView(R.layout.catalogo);//adfkladf
+
+
 
 
     }
