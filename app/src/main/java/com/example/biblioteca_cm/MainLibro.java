92a9c63c1fbb4ad6f817c8fdeea4c44397d0bfeb
Carmen García Muñoz <cargarmun5@alum.us.es>
2024-04-05 18:50:44 +0200

    por resolver

---
+++ app/src/main/java/com/example/biblioteca_cm/MainLibro.java
@@ -0,0 +1,43 @@
+package com.example.biblioteca_cm;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainLibro extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.libro);
+
+        // Inicializar Firebase y Firestore
+        FirebaseApp.initializeApp(this);
+        db = FirebaseFirestore.getInstance();
+
+        // Ocultar ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+    TextView volverCatalogo = findViewById(R.id.volverLibro);
+        volverCatalogo.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View w){
+            logout();
+        }
+    });
+
+
+
+}
